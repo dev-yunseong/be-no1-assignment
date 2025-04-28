d699b9d39d631b4fb761c01ea2d58fbeb7d0508d
Yunseong <dev.yunseong@gmail.com>
2025-04-28 20:00:20 +0900

    클래스를 적용해서 연산 수행하는 계산기 구현

--- src/calculator/App2.java
+++ src/calculator/App2.java
@@ -1,2 +1,34 @@
-package calculator;public class App2 {
+package calculator;
+
+import java.util.Scanner;
+
+public class App2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        int num1, num2;
+        while (true){
+            try {
+                System.out.print("num1: ");
+                num1 = scanner.nextInt();
+                System.out.print("num2: ");
+                num2 = scanner.nextInt();
+
+                System.out.print("operator: ");
+                char operator = scanner.next().charAt(0);
+                calculator.calculate(num1, num2, operator);
+
+                int result = calculator.getResults().getLast();
+
+                System.out.printf("result: %d\n", result);
+            } catch (Exception e) {
+                System.out.println("error : " + e.getMessage());
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String commend = scanner.next();
+            if (commend.equals("exit"))
+                break;
+        }
+    }
 }
--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,2 +1,55 @@
-package calculator;public class Calculator {
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    private List<Integer> results = new ArrayList<>();
+
+    public List<Integer> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Integer> results) {
+        this.results = results;
+    }
+
+    public void removeResult(){
+        results.remove(0);
+    }
+
+    private int add(int num1, int num2){
+        return num1 + num2;
+    }
+    private int sub(int num1, int num2){
+        return num1 - num2;
+    }
+    private int mul(int num1, int num2){
+        return num1 * num2;
+    }
+    private int div(int num1, int num2){
+        return num1 / num2;
+    }
+
+    public int calculate(int num1, int num2, char operator) throws Exception {
+        int result = -1;
+        switch (operator) {
+            case '+':
+                result = add(num1, num2);
+                break;
+            case '-':
+                result = sub(num1, num2);
+                break;
+            case '*':
+                result = mul(num1, num2);
+                break;
+            case '/':
+                if (num2 == 0)
+                    throw new Exception("divided by zero");
+                result = div(num1, num2);
+                break;
+        }
+        results.add(result);
+        return result;
+    }
 }
