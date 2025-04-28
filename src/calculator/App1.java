3c47067180d4b2f84cbd50ac4da41442e8d500bc
Yunseong <dev.yunseong@gmail.com>
2025-04-28 19:59:45 +0900

    클래스를 적용하지 않고 기본 연산을 수행하는 계산기 구현

--- src/calculator/App1.java
+++ src/calculator/App1.java
@@ -1,2 +1,46 @@
-package calculator;public class App1 {
+package calculator;
+
+import java.util.Scanner;
+
+public class App1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1, num2;
+        while (true){
+            try {
+                System.out.print("num1: ");
+                num1 = scanner.nextInt();
+                System.out.print("num2: ");
+                num2 = scanner.nextInt();
+
+                System.out.print("operator: ");
+                char operator = scanner.next().charAt(0);
+                int result = -1;
+                switch (operator) {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        if (num2 == 0)
+                            throw new Exception("divided by zero");
+                        result = num1 / num2;
+                        break;
+                }
+                System.out.printf("result: %d\n", result);
+            } catch (Exception e) {
+                System.out.println("error : " + e.getMessage());
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String commend = scanner.next();
+            if (commend.equals("exit"))
+                break;
+        }
+    }
 }
